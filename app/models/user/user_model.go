301c0b3c09d5c9a4e66bac3d7fe26e4aa5995052
Dwelin <rehun@foxmail.com>
2022-09-28 10:43:00 +0800

    手机号+短信验证码注册

--- app/models/user/user_model.go
+++ app/models/user/user_model.go
@@ -1,16 +1,24 @@
 // Package user 存放用户 Model 相关逻辑
 package user
 
-import "gohub/app/models"
+import (
+	"gohub/app/models"
+	"gohub/pkg/database"
+)
 
 // User 用户模型
 type User struct {
 	models.BaseModel
 
 	Name     string `json:"name,omitempty"`
-	Email    string `json:"_"`
-	Phone    string `json:"_"`
-	password string `json:"_"`
+	Email    string `json:"email,omitempty"`
+	Phone    string `json:"phone,omitempty"`
+	Password string `json:"password,omitempty"`
 
 	models.CommonTimestampsField
+}
+
+// Create 创建用户，通过 User.ID 来判断是否创建成功
+func (userModel *User) Create() {
+	database.DB.Create(&userModel)
 }
