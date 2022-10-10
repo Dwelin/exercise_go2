97a7da51f9aa70bcab3124150999d61804ee03c0
Dwelin <rehun@foxmail.com>
2022-10-10 10:52:24 +0800

    make migration 命令

--- app/cmd/make/make.go
+++ app/cmd/make/make.go
@@ -92,7 +92,7 @@ func createFileFromStub(filePath string, stuName string, model Model, variables 
 
 	// 实现最后一个参数可选
 	replaces := make(map[string]string)
-	if len(replaces) > 0 {
+	if len(variables) > 0 {
 		replaces = variables[0].(map[string]string)
 	}
 
