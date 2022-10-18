ad3640f4646b9923cc1ed8c240fa2803542c8af1
Dwelin <rehun@foxmail.com>
2022-10-18 09:47:11 +0800

    上传用户头像

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -2,9 +2,16 @@
 package file
 
 import (
+	"fmt"
+	"gohub/pkg/app"
+	"gohub/pkg/auth"
+	"gohub/pkg/helpers"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 // Put 将数据存入文件
@@ -26,4 +33,25 @@ func Exists(fileToCheck string) bool {
 
 func FileNameWithoutExtension(fileName string) string {
 	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
+}
+
+func SaveUploadAvatar(c *gin.Context, file *multipart.FileHeader) (string, error) {
+
+	var avatar string
+	// 确保目录存在，不存在创建
+	publicPath := "public"
+	dirName := fmt.Sprintf("/uploads/avatars/%s/%s", app.TimenowInTimezone().Format("2006/01/02"), auth.CurrentUID(c))
+	os.MkdirAll(publicPath+dirName, 0755)
+
+	// 保存文件
+	fileName := randomNameFormUploadFile(file)
+	avatarPath := publicPath + dirName + fileName
+	if err := c.SaveUploadedFile(file, avatarPath); err != nil {
+		return avatar, err
+	}
+	return avatarPath, nil
+}
+
+func randomNameFormUploadFile(file *multipart.FileHeader) string {
+	return helpers.RandomString(16) + filepath.Ext(file.Filename)
 }
